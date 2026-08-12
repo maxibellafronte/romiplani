23a684eff7117fe9fafb9d972ee2198415909526
maxibellafronte <maxil.bellafronte@gmail.com>
2026-08-12 10:40:22 -0300

    Update main.jsx

--- crossfit-prog/src/main.jsx
+++ crossfit-prog/src/main.jsx
@@ -5,16 +5,6 @@ import { inject } from '@vercel/analytics'
 
 inject()
 
-// Google Analytics
-const script = document.createElement('script')
-script.async = true
-script.src = 'https://www.googletagmanager.com/gtag/js?id=G-V4S7V452CQ'
-document.head.appendChild(script)
-window.dataLayer = window.dataLayer || []
-function gtag(){window.dataLayer.push(arguments)}
-gtag('js', new Date())
-gtag('config', 'G-V4S7V452CQ')
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
